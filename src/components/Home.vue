6c95989a0c5ef12a3af5aca1f4b92c7495ed449c
wangfan <yswfgo@qq.com>
2020-12-30 21:37:25 +0800

    完成用户列表的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,154 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+  <el-container class="homeContainer">
+    <!-- 头部 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button></el-header
+    >
+    <!-- 主体部分 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          unique-opened
+          :collapse-transition="false"
+          :collapse="isCollapse"
+          :router="true"
+          :default-active="activePath"
+        >
+        <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单模板 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+              <el-menu-item :index="'/'+son.path" v-for="son in item.children" :key="son.id"
+              @click="saveNavState('/'+son.path)">
+                <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-menu"></i>
+              <!-- 文本 -->
+              <span>{{son.authName}}</span>
+            </template>
+              </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧主体 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 左侧菜单数据
+      menulist: [],
+      // 图标
+      iconObj: {
+        125: 'el-icon-s-custom',
+        103: 'el-icon-s-check',
+        101: 'el-icon-s-shop',
+        102: 'el-icon-s-order',
+        145: 'el-icon-date'
+      },
+      // 是否折叠
+      isCollapse: false,
+      // 路由hash
+      activePath: ''
+    }
+  },
+  created () {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    // 获取所有菜单
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.console.error(res.mata.msg)
+      this.menulist = res.data
+      // console.log(res)
+    },
+    // 菜单折叠
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    // 保存连接激活状态
+    saveNavState (activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = window.sessionStorage.getItem('activePath')
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.homeContainer {
+  height: 100%;
+}
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #373d41;
+  padding-left: 0;
+  color: #ffffff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    img {
+      height: 50px;
+      width: auto;
+      background-color: #cccccc;
+      border-radius: 50%;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu-item{
+    border-right: none;
+    min-width: 199px;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+i{
+  margin-right: 10px;
+}
+.toggle-button{
+  background-color: #4a5064;
+  color: #ffffff;
+  font-size: 10px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
